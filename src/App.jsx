f6570cfc68f864a547cc69c0f0c5a68a3bc12e30
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-10-01 10:16:39 -0430

    organized by folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import TaskList from "./componentes/TaskList";
-import TaskForm from "./componentes/TaskForm";
-import { tasks as data } from "./componentes/tasks";
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import { tasks as data } from "./data/tasks";
 import { useState, useEffect } from "react";
 
 function App() {
