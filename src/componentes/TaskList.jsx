199bcda48a82fe93955366bd1def56d55001a048
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-09-30 21:02:49 -0430

    Tasks and adding tasks to list

--- src/componentes/TaskList.jsx
+++ src/componentes/TaskList.jsx
@@ -1,7 +1,27 @@
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
+
 function TaskList() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  if (tasks.length === 0) {
+    return <h1>No hay tareas aun...</h1>;
+  }
+
   return (
-    <h1>TaskList</h1>
-  )
+    <div>
+      {tasks.map((tasks) => (
+        <div key={tasks.id}>
+          <h1> {tasks.title} </h1>
+          <p> {tasks.Description} </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default TaskList
+export default TaskList;
