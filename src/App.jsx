f200c40c4c03cf6e380d4b309c5bc42fdf21957b
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-09-30 22:37:31 -0430

    change data to app.jsx for sending it to tasklist

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import TaskList from "./componentes/TaskList";
 import TaskForm from "./componentes/TaskForm";
+import { tasks as data } from "./componentes/tasks";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
   return (
     <>
       <TaskForm />
-      <TaskList />
+      <TaskList tasks={tasks} />
     </>
   );
 }
--- src/componentes/TaskList.jsx
+++ src/componentes/TaskList.jsx
@@ -1,20 +1,11 @@
-import { tasks as data } from "./tasks";
-import { useState, useEffect } from "react";
-
-function TaskList() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data);
-  }, []);
-
+function TaskList(props) {
   if (tasks.length === 0) {
     return <h1>No hay tareas aun...</h1>;
   }
 
   return (
     <div>
-      {tasks.map((tasks) => (
+      {props.tasks.map((tasks) => (
         <div key={tasks.id}>
           <h1> {tasks.title} </h1>
           <p> {tasks.Description} </p>
