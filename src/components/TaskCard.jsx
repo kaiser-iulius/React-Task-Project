ef48cb8e08a692d30c274ee1570e26255f76b899
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-10-01 13:10:18 -0430

    using context on taskcard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,19 @@
-function Taskcard({ task, deleteTask }) {
+import { useContext } from "react";
+import { TaskContext } from "../context/Context";
 
+function Taskcard({ task, deleteTask }) {
+  useContext(TaskContext);
   return (
     <div>
       <h1> {task.title} </h1>
       <p> {task.Description} </p>
-      <button onClick={() => {deleteTask(task.id)}}>Delete task</button>
+      <button
+        onClick={() => {
+          deleteTask(task.id);
+        }}
+      >
+        Delete task
+      </button>
     </div>
   );
 }
