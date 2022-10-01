8a8a0a45931205eaaddfef62a75d6f241b85d203
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-10-01 11:45:39 -0430

    deleting task option

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,10 @@
-function Taskcard({ task }) {
+function Taskcard({ task, deleteTask }) {
+
   return (
     <div>
       <h1> {task.title} </h1>
       <p> {task.Description} </p>
+      <button onClick={() => {deleteTask(task.id)}}>Delete task</button>
     </div>
   );
 }
