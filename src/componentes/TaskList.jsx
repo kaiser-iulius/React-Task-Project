8da1852ea04a19ce965c44e063229d54adbce8fd
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-10-01 09:30:38 -0430

    changed props for tasks

--- src/componentes/TaskList.jsx
+++ src/componentes/TaskList.jsx
@@ -1,11 +1,11 @@
-function TaskList(props) {
-  if (props.tasks.length === 0) {
+function TaskList({tasks}) {
+  if (tasks.length === 0) {
     return <h1>No hay tareas aun...</h1>;
   }
 
   return (
     <div>
-      {props.tasks.map((tasks) => (
+      {tasks.map((tasks) => (
         <div key={tasks.id}>
           <h1> {tasks.title} </h1>
           <p> {tasks.Description} </p>
