de58550a04cdc805cf534b41e12d1bf8b3c6ccba
Julio Jose Adam Martinez <julioblogs1998@gmail.com>
2022-10-02 13:23:50 -0430

    changing the usestate position xd

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -3,28 +3,28 @@ import { tasks as data } from "../data/tasks";
 
 export const TaskContext = createContext();
 
-const [tasks, setTasks] = useState([]);
-
-function createTask(task) {
-  setTasks([
-    ...tasks,
-    {
-      id: tasks.length,
-      title: task.title,
-      Description: task.description,
-    },
-  ]);
-}
-
-function deleteTask(taskId) {
-  setTasks(tasks.filter((task) => task.id !== taskId));
-}
-
-useEffect(() => {
-  setTasks(data);
-}, []);
-
 export function ContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
+
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        id: tasks.length,
+        title: task.title,
+        Description: task.description,
+      },
+    ]);
+  }
+
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
   return (
     <TaskContext.Provider value={{ tasks, createTask, deleteTask }}>
       {props.children}
